94909484d146f9ce7754776f9e44e246b3c7faf7
Anon <anonymous23261@icloud.com>
2024-11-05 17:13:15 -0700

    Update text_utils.py

--- text_utils.py
+++ text_utils.py
@@ -5,10 +5,4 @@ def reverse_string(s):
 def capitalize_string(s):
     return s.capitalize()
 
-# Main.py
-# Task: Import text_utils with an alias and utilize its functions
-import text_utils as tu
 
-string = input("Enter a string: ")
-print("Reversed string:", tu.reverse_string(string))
-print("Capitalized string:", tu.capitalize_string(string))
