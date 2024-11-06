5539c306fef6fcef83fb1803eee889752c21203e
Anon <anonymous23261@icloud.com>
2024-11-05 17:13:50 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ import mood_responses
 
 mood = input("How are you feeling today? ")
 print(mood_responses.mood_response(mood))
+
+import text_utils as tu
+
+string = input("Enter a string: ")
+print("Reversed string:", tu.reverse_string(string))
+print("Capitalized string:", tu.capitalize_string(string))
